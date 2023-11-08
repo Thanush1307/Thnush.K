36e9091e026ae5ea935c93d91d0e809cd73129b5
Thanush1307 <thanushkumar1307@gmail.com>
2023-11-08 12:23:27 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
   return(
   <div className="flex space-x-4 my-4">
   <Button  color="green">Green</Button>
-  <Button olor="red">Red</Button>
+  <Button color="red">Red</Button>
   <Button  color="blue">Blue</Button>
   <Button color="yellow">Yellow</Button>
   </div>
