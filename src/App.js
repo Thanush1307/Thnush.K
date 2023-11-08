6e59e1df2763b2a981a2ccd785d49353d6598f6b
Thanush1307 <thanushkumar1307@gmail.com>
2023-11-08 10:30:25 +0530

    mm

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import Navbar from './components/Navbar';
-import Main from './components/Main';
-import Footer from './components/Footer';
+import Navbar from './day2/Navbar';
+import Main from './day2/Main';
+import Footer from './day2/Footer';
 function App() {
   return(
 
