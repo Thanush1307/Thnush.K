fc59889d8d40f048128b72f5bb5cb327aead3039
Thanush1307 <thanushkumar1307@gmail.com>
2023-11-16 20:58:05 +0530

    Initial Commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <hr></hr>
+      <h1 style={{ backgroundColor: "pink", fontSize: "30px" }}>
+        <center>Saran R</center>
+      </h1>
+      <hr></hr>
+    </div>
+  );
+};
+
+export default NavBar;
--- src/day3/Button.jsx
+++
@@ -1,7 +0,0 @@
-const Button = (props) => {
-    return (
-      <button className={`px-4 py-6 bg-${props.color} rounded-lg`}>{props.children}
-      </button>
-    );
-  };
-  export default Button;
